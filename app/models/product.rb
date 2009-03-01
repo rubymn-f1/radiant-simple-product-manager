aa13218079a719ba07365e9fbd9fbd47d8281f84
Andy Atkinson <andyatkinson@gmail.com>
2009-02-28 22:32:10 -0600

    testing change

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ActiveRecord::Base
 	belongs_to :category
+	def paypal_url(return_url)
+    values = {
+      :business => 'seller_1229899173_biz@railscasts.com',
+      :cmd => '_cart',
+      :upload => 1,
+      :return => return_url,
+      :invoice => id
+    }
+    values.merge!({
+      "amount_#{index+1}" => self.price,
+      "item_name_#{index+1}" => self.title,
+      "item_number_#{index+1}" => self.id
+    })
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+  end
 end
