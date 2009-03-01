3904d637e1b8a6788680f9201d4533cee27bde0d
John Reilly <jr@trms.com>
2009-02-28 21:41:01 -0600

    added tags for courses

--- lib/simple_product_manager_tag.rb
+++ lib/simple_product_manager_tag.rb
@@ -137,4 +137,103 @@ module SimpleProductManagerTag
     category = tag.locals.category
     html_escape category.description
   end
+  
+  tag 'courses' do |tag|
+    tag.expand
+  end
+  
+  tag 'course' do |tag|
+       tag.expand
+  end
+  
+  desc "Find a specific course using the SQL conditions specified by 'where'"  
+  tag 'course:find' do |tag|
+    attr = tag.attr.symbolize_keys
+    where=attr[:where]
+    product=Course.find(:first, :conditions => where)
+    if course then
+       tag.locals.course = course
+    end
+    tag.expand
+  end
+  
+  desc "Iterate over all courses in the system, optionally sorted by the field specified by 'order', or constrained by 'where'."
+  tag 'courses:each' do |tag|
+    attr = tag.attr.symbolize_keys
+    order=attr[:order] || 'title ASC'
+    where=attr[:where]
+    result = []
+    if tag.locals.category then
+       courses=tag.locals.category.courses.find(:all, :conditions => where, :order => order)
+    else
+           courses=Course.find(:all, :conditions => where, :order => order)
+         end
+    courses.each do |c|
+      tag.locals.course = c
+      result << tag.expand
+    end
+    result
+  end
+  
+  desc "Renders the ID of the current course loaded by <r:course> or <r:courses:each>"
+  tag 'course:id' do |tag|
+    course = tag.locals.course
+    html_escape course.id
+  end
+  
+ 	desc "Renders the HTML-escaped title of the current course loaded by <r:course> or <r:courses:each>"
+  tag 'course:title' do |tag|
+    course = tag.locals.course
+    html_escape course.title
+  end
+  
+ 	desc "Renders the HTML-escaped description of the current course loaded by <r:course> or <r:courses:each>"
+  tag 'course:description' do |tag|
+    course = tag.locals.course
+    html_escape course.description
+  end
+  
+ 	desc "Renders the HTML-escaped capacity of the current course loaded by <r:course> or <r:courses:each>"
+  tag 'course:capacity' do |tag|
+    course = tag.locals.course
+    html_escape course.capacity
+  end
+  
+ 	desc "Renders the HTML-escaped start_date of the current course loaded by <r:course> or <r:courses:each>"
+  tag 'course:start_date' do |tag|
+    course = tag.locals.course
+    html_escape course.start_date
+  end
+  
+ 	desc "Renders the price of the current course loaded by <r:course> or <r:courses:each>. Formatting can be specified by 'precision', 'unit', 'separator' and 'delimiter'"
+  tag 'course:price' do |tag|
+	  attr = tag.attr.symbolize_keys
+    course = tag.locals.course
+    precision=attr[:precision]
+    if precision.nil? then
+    	precision=2
+    else
+    	precision=precision.to_i
+    end
+    number_to_currency(course.price.to_f, 
+                       :precision => precision,
+                       :unit => attr[:unit] || "$",
+                       :separator => attr[:separator] || ".",
+                       :delimiter => attr[:delimiter] || ",")
+  end
+  
+  desc "Renders an <img> element for the current course loaded by <r:course> or <r:courses:each>.. Optionally takes 'width' and 'height'."
+  tag 'course:image' do |tag|
+	  attr = tag.attr.symbolize_keys
+    course = tag.locals.course
+    image_tag course.photo, :alt => course.title, :size => "#{attr[:width]}x#{attr[:height]}"
+  end
+  
+  desc "Renders the photo URL of the current course loaded by <r:course> or <r:courses:each>."
+  tag 'course:photo_url' do |tag|
+  	course = tag.locals.course
+  	course.photo
+  end
+  
+  
 end
